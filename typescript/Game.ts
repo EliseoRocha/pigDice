6c2e935a02ade8e177a3d1a1cd496bd82277b1ed
Eliseo Rocha <eliseorocha@Eliseos-MacBook-Pro.local>
2018-04-26 15:01:23 -0700

    Setting up TS

---
+++ typescript/Game.ts
@@ -0,0 +1,10 @@
+//put game class here
+
+/**
+ * Gets an integer random value
+ * @param minValue The inclusive min value
+ * @param maxValue The inclusive max value
+ */
+function getRandomIntegerValue(minValue:number, maxValue:number):number{
+    return Math.floor(Math.random() * 10 + 1);
+}
